65fe4f8adcabc7f213b725f7c092202315b09d76
Jacob Chademwiri <jchademwiri@gmail.com>
2024-03-14 08:55:53 +0200

    Coming soon update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import ComingSoon from "@/components/ComingSoon";
-import { LampHero } from "@/components/LampHero";
-import { TestimonialCards } from "@/components/Testimonials";
+// import { LampHero } from "@/components/LampHero";
+// import { TestimonialCards } from "@/components/Testimonials";
 
 const Home = () => {
   return (
     <main>
-      {/* <ComingSoon /> */}
-      <LampHero />
-      <TestimonialCards />
+      <ComingSoon />
+      {/* <LampHero /> */}
+      {/* <TestimonialCards /> */}
     </main>
   );
 };
--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,42 +1,43 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const ComingSoon = () => {
   return (
-    <section className='grid h-screen place-items-center text-center mx-10 '>
+    <section className="grid h-screen place-items-center text-center mx-10 ">
       <div>
-        <h1 className='md:text-7xl text-4xl font-bold py-6'>
-          UNDER <br /> CONSTRUCTION
+        <h1 className="md:text-7xl text-4xl font-bold py-6">
+          SITE UNDER <br /> CONSTRUCTION
         </h1>
-        <p className='md:text-4xl text-2xl py-2 md:py-4  font-thin tracking-wider'>
-          SITE NEARLY READY
+        <p className="md:text-4xl text-2xl py-2 md:py-4  font-thin tracking-wider">
+          WE ARE NEARLY READY
         </p>
-        <div className='h-2 rounded-full w-full bg-accent my-2'>
-          <div className='h-2 rounded-full w-1/2 bg-primary'></div>
+        <div className="h-2 rounded-full w-full bg-accent my-2">
+          <div className="h-2 rounded-full w-1/4 bg-primary"></div>
         </div>
-        <h2 className='py-2 md:text-3xl text-xl font-semibold'>
+        <h2 className="py-2 md:text-3xl text-xl font-semibold">
           Contact us Now
         </h2>
-        <div className='py-2 md:py-4 grid md:grid-cols-2'>
-          <Link className='py-1 hover:text-primary' href='tel:+27796787198'>
-            +27 (0)79 678 7198
+        <div className="py-2 md:py-4 grid md:grid-cols-2">
+          <Link className="py-1 hover:text-primary" href="tel:+27657236771">
+            +27 (0)65 723 6771
           </Link>
           <Link
-            className='py-1 hover:text-primary'
-            href='mailto:info@chichikgadi.co.za'
+            className="py-1 hover:text-primary"
+            href="https://wa.me/27657236771"
+            title="Let's Chat on Whatsapp: +27657236771"
           >
-            info@chichikgadi.co.za
+            Let&apos;s Chat
           </Link>
         </div>
       </div>
-      <footer className='md:my-5 my-10 py-5'>
+      <footer className="md:my-5 my-10 py-5">
         <p>
-          © Developed by{' '}
+          © Developed by{" "}
           <Link
-            rel='noreferrer'
-            target='_blank'
-            title='We Would Love to work with your business, call us now '
+            rel="noreferrer"
+            target="_blank"
+            title="We Would Love to work with your business, call us now "
             href={`https://www.jacobc.co.za/`}
-            className='text-primary'
+            className="text-primary"
           >
             Playhouse Media Group
           </Link>
