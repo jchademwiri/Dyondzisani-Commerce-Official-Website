f1955ddf7a753b1d0594cf62ac2edce3abbd66ef
Jacob Chademwiri <jchademwiri@gmail.com>
2024-03-21 02:07:11 +0200

    Service section

--- src/components/Sections/Services.tsx
+++ src/components/Sections/Services.tsx
@@ -8,7 +8,7 @@ import { motion } from "framer-motion";
 
 const Services = () => {
   return (
-    <section id="services" className="container mx-auto min-h-[50vh]">
+    <section id="services" className="container  mx-auto min-h-[50vh]">
       <motion.header
         initial={{ opacity: 0.0, y: 30 }}
         whileInView={{ opacity: 1, y: 0 }}
@@ -30,51 +30,90 @@ const Services = () => {
         </p>
       </motion.header>
       <div className="my-5 grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        <ServiceCard
-          image={accounting}
-          title="Accounting"
-          alt="accounting lessons"
-          link="/accounting"
-          description="Accounting is the process of recording financial transactions and
+        <motion.div
+          initial={{ opacity: 0.0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.1,
+            duration: 0.5,
+            ease: "easeInOut",
+          }}
+        >
+          <ServiceCard
+            image={accounting}
+            title="Accounting"
+            alt="accounting lessons"
+            link="/accounting"
+            description="Accounting is the process of recording financial transactions and
           information that take place in a business. Learners will be exposed
           to an in depth understanding of financial and managerial accounting,
           the importance corporate governance, Ethics and Auditing."
-        />
-
-        <ServiceCard
-          image={business}
-          title="Business Studies"
-          alt="business studies lessons"
-          link="/business"
-          description="Business studies is board study that focuses on various aspects of running a business. It combines economics and accounting with more emphasis on management and creative strategy."
-        />
-
-        <ServiceCard
-          image={economics}
-          title="Economics Studies"
-          alt="economics studies lessons"
-          link="/economics"
-          description="Economics is a social science that focuses on the production, distribution, and consumption of goods and services. The focus is analysing the choices that individuals, businesses, governments, and global economy make to allocate limited resources."
-        />
-
-        <Card className="hidden sm:block lg:hidden ">
-          <Image
-            src={accounting}
-            alt="Accounting-lessons"
-            className="w-full rounded-t-lg bg-cover object-cover"
           />
-          <CardHeader>
-            <CardTitle className="hover:text-primary">
-              <Link href={"/"}>Accounting</Link>
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            Accounting is the process of recording financial transactions and
-            information that take place in a business. Learners will be exposed
-            to an in depth understanding of financial and managerial accounting,
-            the importance corporate governance, Ethics and Auditing.{" "}
-          </CardContent>
-        </Card>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0.0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.2,
+            duration: 0.5,
+            ease: "easeInOut",
+          }}
+        >
+          <ServiceCard
+            image={business}
+            title="Business Studies"
+            alt="business studies lessons"
+            link="/business"
+            description="Business studies is board study that focuses on various aspects of running a business. It combines economics and accounting with more emphasis on management and creative strategy."
+          />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0.0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.5,
+            ease: "easeInOut",
+          }}
+        >
+          <ServiceCard
+            image={economics}
+            title="Economics Studies"
+            alt="economics studies lessons"
+            link="/economics"
+            description="Economics is a social science that focuses on the production, distribution, and consumption of goods and services. The focus is analysing the choices that individuals, businesses, governments, and global economy make to allocate limited resources."
+          />
+        </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0.0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.4,
+            duration: 0.5,
+            ease: "easeInOut",
+          }}
+        >
+          <Card className="hidden sm:block lg:hidden ">
+            <Image
+              src={accounting}
+              alt="Accounting-lessons"
+              className="w-full rounded-t-lg bg-cover object-cover"
+            />
+            <CardHeader>
+              <CardTitle className="hover:text-primary">
+                <Link href={"/"}>Accounting</Link>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              Accounting is the process of recording financial transactions and
+              information that take place in a business. Learners will be
+              exposed to an in depth understanding of financial and managerial
+              accounting, the importance corporate governance, Ethics and
+              Auditing.{" "}
+            </CardContent>
+          </Card>
+        </motion.div>
       </div>
     </section>
   );
