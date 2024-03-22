bbbcb50502c14f0a797ac09d339475f99cac2611
Jacob Chademwiri <jchademwiri@gmail.com>
2024-03-22 05:37:35 +0200

    Not Found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,26 +1,28 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const NotFound = () => {
   return (
     <main className="grid min-h-full place-items-center  px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">
+        <p className="text-2xl font-semibold tracking-tight text-destructive">
+          Error 404
+        </p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight  sm:text-5xl">
           Page not found
         </h1>
-        <p className="mt-6 text-base leading-7 text-gray-600">
-          Sorry, we couldn’t find the page you’re looking for.
+        <p className="mt-6 text-base leading-7 text-foreground/50">
+          Sorry, we couldn&apos;t find the page you&apos;re looking for.
         </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="/"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Go back home
-          </a>
-          <a href="/" className="text-sm font-semibold text-gray-200">
-            Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
+        <div className="mt-10 flex items-center justify-center gap-x-2">
+          <Link href={"/"}>
+            <Button>Go back home</Button>
+          </Link>
+          <Link href={"/contact"}>
+            <Button variant={"link"} className="flex gap-2 text-foreground">
+              Contact support <span aria-hidden="true">&rarr;</span>
+            </Button>
+          </Link>
         </div>
       </div>
     </main>
