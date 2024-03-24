42096cc3d1978ba5602b3dc5cb1d477070a03c5a
Jacob Chademwiri <jchademwiri@gmail.com>
2024-03-24 08:07:18 +0200

    Removed Prefix data

--- src/app/actions/SendEmail.ts
+++ src/app/actions/SendEmail.ts
@@ -3,25 +3,35 @@ import { DyondzisaniCommerceTemplate } from "@/components/EmailTemplates";
 import { TFormSchema } from "@/lib/validation";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
-export const SendEmail = async (data: TFormSchema) => {
-  console.log(data);
+export const SendEmail = async ({
+  firstName,
+  lastName,
+  email,
+  phone,
+  learnerFullName,
+  learnerGrade,
+  curriculum,
+  subjects,
+  medicalCondition,
+}: TFormSchema) => {
   try {
     const resend: any = new Resend(process.env.RESEND_API_KEY);
     const emailBody = await resend.emails.send({
       from: "Dyodzisani Commerce Website <enquiries@dyondzisanicommerce.co.za>",
-      to: ["info@dyondzisanicommerce.co.za", "jchademwiri@gmail.com"],
+      to: ["info@dyondzisanicommerce.co.za"],
+      cc: { email },
       replyTo: ["info@dyondzisanicommerce.co.za"],
       subject: "Local Dev Test: New Student Enrolment",
       react: DyondzisaniCommerceTemplate({
-        firstName: data.firstName,
-        lastName: data.lastName,
-        email: data.email,
-        phone: data.phone,
-        learnerFullName: data.learnerFullName,
-        learnerGrade: data.learnerGrade,
-        curriculum: data.curriculum,
-        subjects: data.subjects,
-        medicalCondition: data.medicalCondition,
+        firstName,
+        lastName,
+        email,
+        phone,
+        learnerFullName,
+        learnerGrade,
+        curriculum,
+        subjects,
+        medicalCondition,
       }),
     });
     return console.log("Server Action:", emailBody);
